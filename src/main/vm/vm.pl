00bc9a6507fd2022691e3ffd829b56fdb986ecc3
tinta2510 <tatrungtin2004@gmail.com>
2025-05-11 04:04:47 +0700

    checkpoint

--- src/main/vm/vm.pl
+++ src/main/vm/vm.pl
@@ -1,7 +1,7 @@
 %main program
 reduce_prog([Var,Func,Body]) :-
 	% Add vars/consts to the symbol table
-	create_env(Var,env([[]],false),Env),
+	create_env(Var,env([[]],_),Env),
 	% Add functions to the symbol table
 	create_env(Func,Env,Env1),
 	reduce_stmt(config(Body,Env1),_).
@@ -11,8 +11,8 @@ boolean(true).
 boolean(false).
 
 % --- Helper predicates - START ---
-%check if X has been declared in the first list of the environment
-has_declared(X,[id(X,Y,Z,_)|_],id(X,Y,Z,_)):- !.
+%check if X has been declared in a list of the environment
+has_declared(X,[id(X,Y,Z,V)|_],id(X,Y,Z,V)):- !.
 has_declared(X,[_|L],R) :-  has_declared(X,L,R).
 
 % Determine type of the literal
@@ -20,6 +20,23 @@ type_of(V, boolean) :- boolean(V),!.
 type_of(V, integer) :- integer(V),!.
 type_of(V, float) :- float(V),!.
 type_of(V, string) :- string(V),!.
+
+% Loop through the environment to find the identifier
+lookup(Id, [L|_], id(X,Y,Z,V)):-
+	has_declared(Id,L,id(X,Y,Z,V)),!.
+lookup(Id, [_|L2], R):-
+	lookup(Id,L2,R).
+
+% Bind the args to the parameters
+bind_args([],[],Env,Env):-!.
+bind_args([par(Id,_)|Params],[E|Args],Env,EnvOut):-
+	reduce_stmt(config([assign(Id,E)],Env),Env1),
+	bind_args(Params,Args,Env1,EnvOut).
+
+eval_args([],[],_):-!.
+eval_args([E|T],[V|T1],Env):-
+	reduce_all(config(E,Env),config(V,Env)),
+	eval_args(T,T1,Env).
 % --- Helper predicates - END ---
 
 % --- Symbol table - START ---
@@ -28,57 +45,89 @@ type_of(V, string) :- string(V),!.
 create_env([],Env,Env).
 
 %% Variable declaration
-create_env([var(I,type)|_],env([_],_),_):- % Global scope
-	is_builtin(I,_),!,throw(redeclare_identifier(I)).
-create_env([var(I,type)|_],env([L1|_],_),_):-
-	has_declared(I,L1,_),!,throw(redeclare_identifier(I)).
-create_env([var(I,type)|L],env([L1|L2],T),EnvOut):- 
-	create_env(L,env([[id(I,var,type,null)|L1]|L2],T),EnvOut).
+create_env([var(I,Type)|_],env([_],_),_):- % Global scope
+	is_builtin(I,_),!,throw(redeclare_identifier(var(I,Type))).
+create_env([var(I,Type)|_],env([L1|_],_),_):-
+	has_declared(I,L1,_),!,throw(redeclare_identifier(var(I,Type))).
+create_env([var(I,Type)|L],env([L1|L2],T),EnvOut):- 
+	create_env(L,env([[id(I,var,Type,null)|L1]|L2],T),EnvOut).
 
 %% Constant declaration
 create_env([const(I,E)|_],env([_],_),_):- % Global scope
-	is_builtin(I,_),!,throw(redeclare_identifier(I)).
+	is_builtin(I,_),!,throw(redeclare_identifier(const(I,E))).
 create_env([const(I,E)|_],env([L1|_],_),_):-
-	has_declared(I,L1,_),!,throw(redeclare_identifier(I)).
+	has_declared(I,L1,_),!,throw(redeclare_identifier(const(I,E))).
 create_env([const(I,E)|L],env([L1|L2],T),EnvOut):-
-	(type_of(E,type) ->% Determine the type of const
-		create_env(L,env([[id(I,const,type,E)|L1]|L2],T),EnvOut)
+	(type_of(E,Type) -> % Determine the type of const
+		create_env(L,env([[id(I,const,Type,E)|L1]|L2],T),EnvOut)
 		; throw(type_mismatch(const(I,E)))).
 
 %% Parameter declaration
-create_env([par(I,type)|_],env([L1|_],_),_):-
-	has_declared(I,L1,_),!,throw(redeclare_identifier(I)).
-create_env([par(I,type)|L],env([L1|L2],T),EnvOut):-
-	create_env(L,env([[id(I,par,type,null)|L1]|L2],T),EnvOut).
+create_env([par(I,Type)|_],env([L1|_],_),_):-
+	has_declared(I,L1,_),!,throw(redeclare_identifier(par(I,Type))).
+create_env([par(I,Type)|L],env([L1|L2],T),EnvOut):-
+	create_env(L,env([[id(I,par,Type,null)|L1]|L2],T),EnvOut).
 
 %% Function declaration
 % Only care Global scope
-create_env([func(I,Params,RetType,Stmts)|_],env([_],_),_):- 
+create_env([func(I,_,_,_)|_],env([_],_),_):- 
 	is_builtin(I,_),!,
 	throw(redeclare_function(I)).
-create_env([func(I,Params,RetType,Stmts)|_],env([L1|_],_),_):-
+create_env([func(I,_,_,_)|_],env([L1|_],_),_):-
 	has_declared(I,L1,_),!,
 	throw(redeclare_function(I)).
-% Add function to the symbol table
+%# Add function to the symbol table
 create_env([func(I,Params,RetType,Stmts)|L],env([L1|L2],T),EnvOut):-
-	create_env(L,env([[id(I,func,RetType,null)|L1]|L2],T),EnvOut1).
+	create_env(
+		L,env([[id(I,func,func(I,Params,RetType,Stmts),null)|L1]|L2],T),
+		EnvOut).
 
 %% Procedure declaration
-create_env([proc(I,Params,Stmts)|_],env([_],_),_):- 
+create_env([proc(I,_,_)|_],env([_],_),_):- 
 	is_builtin(I,_),!,
 	throw(redeclare_procedure(I)).
-create_env([proc(I,Params,Stmts)|_],env([L1|_],_),_):-
+create_env([proc(I,_,_)|_],env([L1|_],_),_):-
 	has_declared(I,L1,_),!,
 	throw(redeclare_procedure(I)).
-% Add procedure to the symbol table
+%# Add procedure to the symbol table
 create_env([proc(I,Params,Stmts)|L],env([L1|L2],T),EnvOut):-
-	create_env(L,env([[id(I,proc,null,null)|L1]|L2],T),EnvOut1).
+	create_env(
+		L,env([[id(I,proc,proc(I,Params,Stmts),null)|L1]|L2],T),
+		EnvOut).
+
+%% Update the symbol table
+update_env(Id,_,env([],_),env(_,_)):-
+	throw(undeclare_identifier(Id)).
+
+update_env(Id,Val,env([Scope|L],CurrFunc),env([ScopeOut|L],CurrFunc)):-
+	update_scope(Id,Val,Scope,ScopeOut,CurrFunc),!.
+
+update_env(Id,Val,env([Scope|L],CurrFunc),env([Scope|LOut],CurrFunc)):-
+	update_env(Id,Val,env(L,_),env(LOut,CurrFunc)).
+
+update_scope(_,_,[],[],_):- fail.
+update_scope(Id,Val,[id(Id,const,_,_)|_],_,_):-
+	% Cannot assign to a constant
+	throw(cannot_assign(assign(Id,Val))).
+update_scope(Id,Val,[id(Id,func,Type,_)|T],[id(Id,func,Type,Val)|T],CurrFunc):-
+	!,nonvar(CurrFunc),CurrFunc == Id,
+	Type = func(_,_,RetType,_),
+	(type_of(Val,RetType) -> true ; throw(type_mismatch(assign(Id,Val)))).
+update_scope(Id,Val,[id(Id,Kind,Type,_)|T],[id(Id,Kind,Type,Val)|T],_):-
+	(Kind == var ; Kind == par),!,
+	(type_of(Val,Type) -> true ; throw(type_mismatch(assign(Id,Val)))).
+update_scope(Id,Val,[H|T],[H|NewT],CurrFunc):-
+	update_scope(Id,Val,T,NewT,CurrFunc).
+
 %% --- Symbol table - END ---
 
 % --- Expression reduction - START ---
 %% --- Numerical expressions - START ---
+reduce(config(sub(E),Env),config(R,Env)) :-  
+	reduce_all(config(E,Env),config(V,Env)),
+	(number(V) -> R is -V ; throw(type_mismatch(sub(E)))).
 % Addition
-reduce(config(add(E1,E2),Env),config(R,Env)) :-  
+reduce(config(add(E1,E2),Env),config(R,Env)) :-
 	reduce_all(config(E1,Env),config(V1,Env)),
 	reduce_all(config(E2,Env),config(V2,Env)),
 	(number(V1), number(V2) -> R is V1 + V2 ; throw(type_mismatch(add(E1,E2)))).
@@ -178,19 +227,74 @@ reduce(config(ne(E1, E2), Env), config(R, Env)) :-
 		(V1 =\= V2 -> R = true ; R = false)
 	; throw(type_mismatch(ne(E1,E2))).
 %% --- Relational expressions - END ---
+
+%% --- Variable, Constant expressions - START ---
+reduce(config(Id,env(L,_)),config(Value,_)):-
+	atom(Id), %??? simple identifier
+	(lookup(Id,L,id(Id,Kind,_,Value)) -> % Lookup in the environment
+		((Kind \= var, Kind \= const, Kind \= par -> 
+		 	throw(undeclared_identifier(Id)) ; true),
+		  (Value == null -> throw(invalid_expression(Id)) ; true)
+		);
+		throw(undeclared_identifier(Id))).
+%% --- Variable, Constant expression - END ---
+
+%% --- Function call - START --- 
+reduce(config(call(F,[]),_),config(X,_)):-
+	is_builtin(F,func),!,
+	p_call_builtin(F,X).
+
+reduce(config(call(F,Args),Env),config(R,EnvOut)):-
+	Env = env(SymTable,_),
+	lookup(F,SymTable,id(F,func,func(F,Params,_,Stmts),_)),!,
+	((length(Args, N),length(Params, M),N==M)-> % Check the number of arguments
+		(
+			eval_args(Args,Args1,Env)), % Evaluate the arguments
+			% Add the parameters to the symbol table
+			create_env(Params,env([[]|SymTable],_),Env1),
+			% Bind the arguments to the parameters
+			bind_args(Params,Args1,Env1,Env2),
+			% Reduce the function body
+			Env2 = env(SymTable1,_),
+			write("HERE: "), write(EnvOut),nl,
+			reduce_stmt(config(Stmts,env(SymTable1,F)),EnvOut),
+			EnvOut = env(SymTable2,_),
+			lookup(F,SymTable2,id(_,_,_,R)),
+			writeln(R)
+		)
+		; throw(wrong_number_of_argument(call(F,Args))).
+	
+reduce(config(call(F,_),_),_):-
+	undeclare_function(F).
 % --- Expression reduction - END ---
 
 
 reduce_all(config(V,Env),config(V,Env)):- (number(V);boolean(V);string(V)),!.
 reduce_all(config(E,Env),config(E2,Env)):-
-		reduce(config(E,Env),config(E1,Env)),!,
-		reduce_all(config(E1,Env),config(E2,Env)).
+	reduce(config(E,Env),config(E1,Env)),!,
+	reduce_all(config(E1,Env),config(E2,Env)).
 
 
 % --- Statement reduction - START ---
-reduce_stmt(config([call(F,[X])|Body],_),_) :- 
-		is_builtin(F,_),!,
-		reduce_all(config(X,Env),config(V,Env)),
-		p_call_builtin(F,[V]), % Read stmt's input
-		reduce_stmt(config(Body,_),_).
+reduce_stmt(config([],Env),Env):- !. % Base case
+%% --- Procedure-Call - START ---
+reduce_stmt(config([call(F,[X])|Body],Env),EnvOut) :- 
+	is_builtin(F,proc),!,
+	reduce_all(config(X,Env),config(V,Env)),
+	p_call_builtin(F,[V]), %!!! Read stmt's input
+	reduce_stmt(config(Body,Env),EnvOut).
+%% --- Procedure-Call - END ---
 
+%% --- Assignment - START ---
+reduce_stmt(config([assign(Id,E)|Body],Env),EnvOut) :-
+	reduce_all(config(E,Env),config(V,Env)),
+	catch(
+		update_env(Id,V,Env,Env1),
+		Error,
+		(
+			Error =.. [ErrName|_],
+			NewError =.. [ErrName|[assign(Id,V)]],
+			throw(NewError)
+		)
+	),
+	reduce_stmt(config(Body,Env1),EnvOut).
--- src/test/VMSuite.py
+++ src/test/VMSuite.py
@@ -3,33 +3,85 @@ from TestUtils import TestVM
 
 
 class VMSuite(unittest.TestCase):
-    def test_401(self):        
-        input = """[[],[],[call(writeInt,[3])]]."""
-        expect = "3"
-        self.assertTrue(TestVM.test(input, expect, 401))
+    # def test_401(self):        
+    #     input = """[[],[],[call(writeInt,[3])]]."""
+    #     expect = "3"
+    #     self.assertTrue(TestVM.test(input, expect, 401))
 
-    def test_402(self):        
-        input = """[[],[],[call(writeInt,[add(3,1)])]]."""
-        expect = "4"
-        self.assertTrue(TestVM.test(input, expect, 402))
+    # def test_402(self):        
+    #     input = """[[],[],[call(writeInt,[add(3,1)])]]."""
+    #     expect = "4"
+    #     self.assertTrue(TestVM.test(input, expect, 402))
 
-    def test_403(self):        
-        input = """[[var(a,integer),var(b,integer),var(a,float)],[],[call(writeInt,[1])]]."""
-        expect = "Redeclared identifier: var(a,float)"
-        self.assertTrue(TestVM.test(input, expect, 403))
+    # def test_403(self):        
+    #     input = """[[var(a,integer),var(b,integer),var(a,float)],[],[call(writeInt,[1])]]."""
+    #     expect = "Redeclared identifier: var(a,float)"
+    #     self.assertTrue(TestVM.test(input, expect, 403))
 
-    def test_404(self):        
-        input = """[[],[],[call(writeInt,[sub(3,1)])]]."""
-        expect = "2"
-        self.assertTrue(TestVM.test(input, expect, 404))
+    # def test_404(self):        
+    #     input = """[[],[],[call(writeInt,[sub(3,1)])]]."""
+    #     expect = "2"
+    #     self.assertTrue(TestVM.test(input, expect, 404))
 
-    def test_405(self):        
-        input = """[[],[],[call(writeInt,[add(4,sub(3,1))])]]."""
-        expect = "6"
-        self.assertTrue(TestVM.test(input, expect, 405))
+    # def test_405(self):        
+    #     input = """[[],[],[call(writeInt,[add(4,sub(3,1))])]]."""
+    #     expect = "6"
+    #     self.assertTrue(TestVM.test(input, expect, 405))
         
-    def test_406(self):
-        input = """[[],[],[call(writeReal,[10.0])]]."""
-        expect = "10.0"
-        self.assertTrue(TestVM.test(input, expect, 406))
+    # def test_406(self):
+    #     input = """[[],[],[call(writeReal,[10.0])]]."""
+    #     expect = "10.0"
+    #     self.assertTrue(TestVM.test(input, expect, 406))
+        
+    # def test_407(self):
+    #     input = """[[const(a,10)],[],[call(writeInt,[a])]]."""
+    #     expect = "10"   
+    #     self.assertTrue(TestVM.test(input, expect, 407))
+        
+    # def test_408(self):
+    #     input = """[[var(a,integer)],[],[call(writeInt,[a])]]."""
+    #     expect = "Invalid expression: a"   
+    #     self.assertTrue(TestVM.test(input, expect, 408))
 
+    # def test_409(self):
+    #     input = """[[],[],[
+    #         call(writeIntLn,[times(3,2)]),
+    #         call(writeRealLn,[rdiv(3,2)]),
+    #         call(writeIntLn,[sub(-10)]),
+    #         call(writeIntLn,[idiv(5,2)]),
+    #         call(writeInt,[imod(5,2)])
+    #     ]]."""
+    #     expect = "6\n1.5\n10\n2\n1"
+    #     self.assertTrue(TestVM.test(input, expect, 409))
+        
+    # def test_410(self):
+    #     input = """[[],[],[
+    #         call(writeBool,[bnot(true)]),
+    #         call(writeBool,[band(true,false)]),
+    #         call(writeBool,[bor(true,false)])
+    #         ]]."""
+    #     expect = "falsefalsetrue"
+    #     self.assertTrue(TestVM.test(input, expect, 410))
+
+    # def test_411(self):
+    #     input = """[[var(a,integer)],[],[assign(a,3),call(writeInt,[a])]]."""
+    #     expect = "3"
+    #     self.assertTrue(TestVM.test(input, expect, 411))
+        
+    # def test_412(self):
+    #     input = """[[const(a,10)],[],[assign(a,3),call(writeInt,[a])]]."""
+    #     expect = "Cannot assign to a constant: assign(a,3)"
+    #     self.assertTrue(TestVM.test(input, expect, 412))
+        
+    # def test_413(self):
+    #     input = """[[var(c,integer),const(b,10),var(a,integer)],[],[assign(a,3),call(writeInt,[a])]]."""
+    #     expect = "3"
+    #     self.assertTrue(TestVM.test(input, expect, 413))
+        
+    def test_414(self):
+        input = """
+[[var(a,integer)],
+[func(foo,[par(a,integer),par(b,integer)],integer,[assign(a,add(a,b)),assign(foo,a)])], 
+[assign(a,3),call(writeIntLn,[call(foo,[a,3])]),call(writeIntLn,[a])]]."""
+        expect = "6\n3"
+        self.assertTrue(TestVM.test(input, expect, 414))
